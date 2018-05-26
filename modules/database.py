a2c08fe7981db022061b476fbab532cec87e8783
ivanlmj <ivanlmj@gmail.com>
2018-05-26 18:49:15 -0500

    fixing login() method, queries + improvements
    - __init__(): improving docstring
    - login() / create_user(): fixing queries (table names)
    - login(): fixing calling for instance variable "db"
    - login(): fixing login validation (fectchone() - tuple pos 0)
    - initial_setup(): adding method for initial database setup

--- modules/database.py
+++ modules/database.py
@@ -6,7 +6,7 @@ import sqlite3
 class SQLite:
 
     def __init__(self):
-        """ Database object setup. """
+        """ Database setup. """
         self.db = sqlite3.connect('puppy.sqlite3')
 
     def login(self, username, password):
@@ -23,14 +23,13 @@ class SQLite:
                 - 2: user not found
         """
         cursor = self.db.cursor()
-        query = """ SELECT PassWord FROM Auth 
-                    WHERE UserName='{0}'
-                """.format(username)
+        query = """ SELECT PassWord FROM Users
+                    WHERE UserName='{0}'""".format(username)
         cursor.execute(query)
         data = cursor.fetchone()
-        db.commit()
+        self.db.commit()
         if data:
-            if password == data:
+            if password == data[0]:
                 return 0
             else:
                 return 1
@@ -46,11 +45,37 @@ class SQLite:
                 - username
                 - password
         """
-        ts = datetime.now()
         cursor = self.db.cursor()
-        query = """ INSERT INTO Auth 
-                    (FullName, UserName, PassWord, Created, Modified) 
-                    VALUES ({0}, {1}, {2}, {3}, {4})
-                """.format(fullname, username, password, ts, ts)
+        query = """ INSERT INTO Users (FullName, UserName, PassWord) VALUES
+             ('{0}', '{1}', '{2}') """.format(fullname, username, password)
         cursor.execute(query)
-        db.commit()
+        self.db.commit()
+
+    def initial_setup(self):
+        """
+            Creates the following tables for initial database setup:
+                - Users:    registered users
+                - Sessions: controlling sessions of authenticated users
+        """
+        cursor = self.db.cursor()
+        drop_users = """ DROP TABLE IF EXISTS Users """
+        drop_sessions = """ DROP TABLE IF EXISTS Sessions """
+        cursor.execute(drop_users)
+        cursor.execute(drop_sessions)
+        users = """ CREATE TABLE Users
+        (Id INTEGER PRIMARY KEY,
+        FullName TEXT, UserName TEXT, PassWord TEXT,
+        CreateTime DATETIME DEFAULT CURRENT_TIMESTAMP,
+        UpdateTime DATETIME DEFAULT CURRENT_TIMESTAMP) """
+        admin = """ INSERT INTO
+        Users (FullName, UserName, Password)
+        VALUES ('Administrator', 'root', 'root')
+        """
+        sessions = """ CREATE TABLE Sessions
+        (Id INTEGER PRIMARY KEY, UserName TEXT,
+        StartTime DATETIME DEFAULT CURRENT_TIMESTAMP,
+        EndTime DATETIME DEFAULT CURRENT_TIMESTAMP) """
+        cursor.execute(users)
+        cursor.execute(admin)
+        cursor.execute(sessions)
+        self.db.commit()
