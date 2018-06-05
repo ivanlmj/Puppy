201bf24e6840532141448918218ff242b265e19e
ivanlmj <ivanlmj@gmail.com>
2018-06-04 21:10:48 -0500

    adding AJAX function for POST request

--- static/ajax.js
+++ static/ajax.js
@@ -11,3 +11,21 @@ function ajaxGet(path, cbFunction) {
     xhttp.send();
 
 };
+
+
+function ajaxPost(path, name, action) {
+
+    var xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+	    alert("Request OK!");
+        } else {
+	    alert("Request NOK...")
+	}
+    };
+    xhttp.open('POST', path, true );
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send(`name=${name}&action=${action}`);
+
+};
