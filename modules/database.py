c4e781f3878fb89fa9d80f8509118e5f32119a4f
ivanlmj <ivanlmj@gmail.com>
2018-07-04 15:11:20 -0500

    improving run_action()
    - adding call for log_action, using additional parameters

--- modules/database.py
+++ modules/database.py
@@ -76,7 +76,7 @@ class SQLite:
         return data
 
 
-    def run_action(self, action_id):
+    def run_action(self, action_id, user):
         """
             Runs an action, based on action_id.
 
@@ -84,10 +84,19 @@ class SQLite:
                 - action_id: action_id for running a specfic action
         """
         cursor = self.db.cursor()
-        query = """ SELECT Action FROM Actions
-                    WHERE Id='{0}'""".format(action_id)
-        result = sp.call(query)
-        return result
+        query_action = """ SELECT Name, Action FROM Actions
+                           WHERE Id='{0}'""".format(action_id)
+        cursor.execute(query_action)
+        action_data   = cursor.fetchall()
+        action_name   = action_data[0]
+        action_result = sp.call(action_data[1])
+        if action_resutlt == 0:
+            query_action_log  = """ INSERT INTO ActionsLog
+            (Name, ReturnCode, RunBy) VALUES
+            ('{0}', '{1}', '{2}')""".format(action_name, action_result, User)
+            cursor.execute(query_action_log)
+        self.db.commit()
+        return action_result
 
 
     def create_action(self, name, action):
