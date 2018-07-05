01b11a5472af91540aa195412ecf06cef76ebbc0
ivanlmj <ivanlmj@gmail.com>
2018-07-05 15:51:47 -0500

    fixing wrong return call for __init__ method
    - defining db connector as instance variable

--- modules/database.py
+++ modules/database.py
@@ -7,4 +7,4 @@ class SQLite:
         """ Database setup. """
         BASE_DIR = os.path.dirname(os.path.abspath(__name__))
         db_path = os.path.join(BASE_DIR, "puppy.sqlite3")
-        return sqlite3.connect(db_path)
+        self.conn = sqlite3.connect(db_path)
