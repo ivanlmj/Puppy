fb56a39c317b26367f18e977bdd9bd5a34e08a1e
ivanlmj <ivanlmj@gmail.com>
2018-07-20 11:42:57 -0500

    prototyping Unit Test class

---
+++ modules/unit_test.py
@@ -0,0 +1,17 @@
+
+import users
+import unittest
+
+user = users.User()
+
+class Probe(unittest.TestCase):
+    """ Probing functionality of multiple classes for Puppy. """
+    
+    def test_user_create(self):
+        self.assertEqual(user.create("Captain Nemo", "nemo", "nemo2000"), 0)
+
+    def test_user_login(self):
+        self.assertEqual(user.login("nemo", "nemo2000"), 0)
+
+    def test_user_remove(self):
+        self.assertEqual(user.remove("nemo"), 0)
