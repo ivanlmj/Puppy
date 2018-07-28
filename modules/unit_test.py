0dc38359b9626978464ea1e5e6a9c7ac05c0e0f5
ivanlmj <ivanlmj@gmail.com>
2018-07-28 16:19:21 -0500

    improving/adding docstrings

--- modules/unit_test.py
+++ modules/unit_test.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python3
+
+""" Module defined for testing multiple aspects from Puppy modules. """
 
 import users
 import unittest
@@ -5,15 +8,18 @@ import unittest
 user = users.User()
 
 class Probe(unittest.TestCase):
-    """ Probing functionality of multiple classes for Puppy. """
+    """ Probing functionalities from Puppy. """
     
     def test_user_create(self):
+        """ Testing user ceation. """
         self.assertEqual(user.create("Captain Nemo", "nemo", "nemo2000"), 0)
 
     def test_user_login(self):
+        """ Testing user login. """
         self.assertEqual(user.login("nemo", "nemo2000"), 0)
 
     def test_user_remove(self):
+        """ Testing user removal. """
         self.assertEqual(user.remove("nemo"), 0)
 
 
