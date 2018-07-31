34fed21c452911aa38885f71dc7094bb513d4f34
ivanlmj <ivanlmj@gmail.com>
2018-07-31 12:47:22 -0500

    improving module structure and functionality
    - reorganizing disposition of methods inside its class
    - improving return definition for every method
    - adding try/except for every method

--- modules/actions.py
+++ modules/actions.py
@@ -10,29 +10,6 @@ class Actions:
         self.db = database.SQLite()
 
 
-    def run(self, action_id, user):
-        """
-            Runs an action, based on action_id.
-
-            Parameters:
-                - action_id: action_id for running a specfic action
-        """
-        cursor = self.db.conn.cursor()
-        query_action = """ SELECT Name, Action FROM Actions
-                           WHERE Id='{0}'""".format(action_id)
-        cursor.execute(query_action)
-        action_data   = cursor.fetchall()
-        action_name   = action_data[0]
-        action_result = sp.call(action_data[1])
-        if action_result == 0:
-            query_action_log  = """ INSERT INTO ActionsLog
-            (Name, ReturnCode, RunBy) VALUES
-            ('{0}', '{1}', '{2}')""".format(action_name, action_result, User)
-            cursor.execute(query_action_log)
-        self.db.conn.commit()
-        return action_result
-
-
     def create(self, name, action):
         """
             Creates a new action.
@@ -41,24 +18,43 @@ class Actions:
                 - name:   action name
                 - action: command that executes the action
         """
-        cursor = self.db.conn.cursor()
-        query = """ INSERT INTO Actions (Name, Action) VALUES
-                    ('{0}', '{1}') """.format(name, action)
-        cursor.execute(query)
-        self.db.conn.commit()
+        try:
+            cursor = self.db.conn.cursor()
+            query = """ INSERT INTO Actions (Name, Action) VALUES
+                        ('{0}', '{1}') """.format(name, action)
+            cursor.execute(query)
+            self.db.conn.commit()
+            return 0
+        except Exception as e:
+            return e
 
 
-    def remove(self, name):
+    def run(self, action_id, user):
         """
-            Removes an action.
+            Runs an action, based on action_id.
 
             Parameters:
-                - name:   action name
+                - action_id: action_id for running a specfic action
         """
-        cursor = self.db.conn.cursor()
-        query = """ DELETE FROM Actions WHERE Name='{0}'""".format(name)
-        cursor.execute(query)
-        self.db.conn.commit()
+        try:
+            cursor = self.db.conn.cursor()
+            query_action = """ SELECT Name, Action FROM Actions
+                               WHERE Id='{0}'""".format(action_id)
+            cursor.execute(query_action)
+            action_data   = cursor.fetchall()
+            action_name   = action_data[0]
+            action_result = sp.call(action_data[1])
+            if action_result == 0:
+                query_action_log  = """ INSERT INTO ActionsLog
+                (Name, ReturnCode, RunBy) VALUES('{0}', '{1}', '{2}')
+                """.format(action_name, action_result, User)
+                cursor.execute(query_action_log)
+                self.db.conn.commit()
+                return 0
+            else:
+                return 1
+        except Exception as e:
+            return e
 
 
     def show(self, action_id=None):
@@ -68,26 +64,56 @@ class Actions:
             Parameters:
                 - action_id (optional): action_id for retrieving specific data
         """
-        cursor = self.db.conn.cursor()
-        query = None
-        if action_id is None:
-            query = """ SELECT * FROM Actions """
-        else:
-            query = """ SELECT * FROM Actions
-                        WHERE Id='{0}'""".format(action_id)
-        cursor.execute(query)
-        data = [ row for row in cursor.fetchall() ]
-        self.db.conn.commit()
-        return data
+        try:
+            cursor = self.db.conn.cursor()
+            query = None
+            if action_id is None:
+                query = """ SELECT * FROM Actions """
+            else:
+                query = """ SELECT * FROM Actions
+                            WHERE Id='{0}'""".format(action_id)
+            cursor.execute(query)
+            data = [ row for row in cursor.fetchall() ]
+            self.db.conn.commit()
+            return data
+        except Exception as e:
+            return e
 
 
     def logged(self):
         """
             List logged actions.
         """
-        cursor = self.db.conn.cursor()
-        query = """ SELECT * FROM ActionsLog """
-        cursor.execute(query)
-        data = cursor.fetchall()
-        self.db.conn.commit()
-        return data
+        try:
+            cursor = self.db.conn.cursor()
+            query = """ SELECT * FROM ActionsLog """
+            cursor.execute(query)
+            data = cursor.fetchall()
+            self.db.conn.commit()
+            return data
+        except Exception as e:
+            return e
+
+
+    def remove(self, name):
+        """
+            Removes an action.
+
+            Parameters:
+                - name:   action name
+        """
+        try:
+            cursor = self.db.conn.cursor()
+            query = """ DELETE FROM Actions WHERE Name='{0}'""".format(name)
+            cursor.execute(query)
+            self.db.conn.commit()
+            query_remove = """ SELECT * FROM Actions
+                               WHERE Name='{0}'""".format(name)
+            cursor.execute(query_remove)
+            data = cursor.fetchall()
+            if len(data) == 0:
+                return 0
+            else:
+                return 1
+        except Exception as e:
+            return e
