0c13cf2b3ac16ba2d79e5162c0cb152e0c39727b
ivanlmj <ivanlmj@gmail.com>
2018-07-31 15:32:46 -0500

    expanding Unit Test for actions module

--- modules/unit_test.py
+++ modules/unit_test.py
@@ -2,14 +2,17 @@
 
 """ Module defined for testing multiple aspects from Puppy modules. """
 
+import actions
 import users
 import unittest
 
+action = actions.Action()
 user = users.User()
 
 class Probe(unittest.TestCase):
     """ Probing functionalities from Puppy. """
     
+    # User() class test
     def test_user_create(self):
         """ Testing user ceation. """
         self.assertEqual(user.create("Captain Nemo", "nemo", "nemo2000"), 0)
@@ -22,6 +25,26 @@ class Probe(unittest.TestCase):
         """ Testing user removal. """
         self.assertEqual(user.remove("nemo"), 0)
 
+    # Action() class test
+    def test_action_create(self):
+        """ Testing action creation. """
+        self.assertEqual(action.create("Create File", "touch /tmp/file"), 0)
+
+    def test_action_run(self):
+        """ Testing action execution. """
+        self.assertEqual(action.run(0, "root"), 0)
+
+    def test_action_show(self):
+        """ Testing display of actions. """
+        self.assertEqual(type(action.show()), list)
+
+    def test_action_logged(self):
+        """ Testing display of logged actions. """
+        self.assertEqual(type(action.logged()), list)
+
+    def test_action_remove(self):
+        """ Testing action removal. """
+        self.assertEqual(action.remove(), 0)
 
 if __name__ == "__main__":
     unittest.main()
