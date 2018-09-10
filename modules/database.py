95eabd827862aec9f42a80feef8d531b84cbc183
ivanlmj <ivanlmj@gmail.com>
2018-09-09 19:36:41 -0500

    removing unnecessary prints

--- modules/database.py
+++ modules/database.py
@@ -9,5 +9,3 @@ class SQLite:
         BASE_DIR = os.path.dirname(os.path.abspath(__name__)) + "/modules"
         self.db_path = os.path.join(BASE_DIR, "puppy.sqlite3")
         self.conn = sqlite3.connect(self.db_path)
-        print("DB PATH:", self.db_path)
-        print("DB CONN:", self.conn)
