2dda6d5054d400922ef04b4ef3f69ac4f23e88b3
ivanlmj <ivanlmj@gmail.com>
2018-09-10 10:06:36 -0500

    fixing column names (and variable names)

--- static/logged_actions.js
+++ static/logged_actions.js
@@ -6,26 +6,27 @@
 
 function buildTable(data) {
     var xhrResponse = JSON.parse(data);
+    console.log(xhrResponse);
 
     var tableDiv = document.getElementById("actions");
     var table = document.createElement('TABLE');
     var header = document.createElement('TR');
 
     var id = document.createElement('TH');
-    var name = document.createElement('TH');
+    var action = document.createElement('TH');
     var command = document.createElement('TH');
-    var create_time = document.createElement('TH');
-    var update_time = document.createElement('TH');
+    var run_by = document.createElement('TH');
+    var run_time = document.createElement('TH');
     id.appendChild(document.createTextNode("Id"));
-    name.appendChild(document.createTextNode("Name"));
-    command.appendChild(document.createTextNode("Command"));
-    create_time.appendChild(document.createTextNode("Create Time"));
-    update_time.appendChild(document.createTextNode("Update Time"));
+    action.appendChild(document.createTextNode("Action"));
+    command.appendChild(document.createTextNode("Status"));
+    run_by.appendChild(document.createTextNode("Run By"));
+    run_time.appendChild(document.createTextNode("Run Time"));
     header.appendChild(id);
-    header.appendChild(name);
+    header.appendChild(action);
     header.appendChild(command);
-    header.appendChild(create_time);
-    header.appendChild(update_time);
+    header.appendChild(run_by);
+    header.appendChild(run_time);
     table.appendChild(header);
 
     if (xhrResponse.length == 0) {
