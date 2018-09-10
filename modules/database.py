9c7e8e7b5edaadaec82eb9d745149a6c3e70a749
ivanlmj <ivanlmj@gmail.com>
2018-09-09 19:03:50 -0500

    fixing path definition for usage of correct db

--- modules/database.py
+++ modules/database.py
@@ -1,3 +1,4 @@
+
 import os.path
 import sqlite3
 
@@ -5,6 +6,13 @@ class SQLite:
 
     def __init__(self):
         """ Database setup. """
-        BASE_DIR = os.path.dirname(os.path.abspath(__name__))
-        db_path = os.path.join(BASE_DIR, "puppy.sqlite3")
-        self.conn = sqlite3.connect(db_path)
+        # add modules path here (+++)
+        #   DB PATH: /home/ivanlmj/git/Puppy/puppy.sqlite3
+        #   DB CONN: <sqlite3.Connection object at 0x7ff07bc31ab0>
+        #   DB Path: /home/ivanlmj/git/Puppy/puppy.sqlite3
+        #   DB Conn: <sqlite3.Connection object at 0x7ff07bc31ab0>
+        BASE_DIR = os.path.dirname(os.path.abspath(__name__)) + "/modules"
+        self.db_path = os.path.join(BASE_DIR, "puppy.sqlite3")
+        self.conn = sqlite3.connect(self.db_path)
+        print("DB PATH:", self.db_path)
+        print("DB CONN:", self.conn)
