ba2a9afffdae2bcca91383a923af704c6054c52f
ivanlmj <ivanlmj@gmail.com>
2018-09-10 00:15:58 -0500

    fixing select button for submitting action run

--- static/actions.js
+++ static/actions.js
@@ -6,12 +6,15 @@
 
 function buildSelect(data) {
     var xhrResponse = JSON.parse(data);
+    console.log(xhrResponse);
 
     var actions = document.getElementById("action");
     for (i = 0; i < xhrResponse.length; i++) {
         var option = document.createElement('OPTION');
-	option.value = xhrResponse[i][1];
+	option.value = xhrResponse[i][0];
+	console.log("Option Value:", option.value);
 	var text = document.createTextNode(xhrResponse[i][1]);
+	console.log("Option Text:", text);
 	option.appendChild(text);
 	actions.appendChild(option);
     }
