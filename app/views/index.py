2f32662933525bad50e4cb20da823daa0a4cb5ba
ivanlmj <ivanlmj@gmail.com>
2018-12-04 18:17:51 -0600

    configuring /index as main path + improvements
    - removing unnecessary modules

--- app/views/index.py
+++ app/views/index.py
@@ -1,7 +1,12 @@
+""" Module for '/' route """
 
-from app import app, redirect
+from app import app, abort, render_template, request
 
-@app.route("/", methods=['GET'])                                                
-@app.route("/index", methods=['GET'])                                           
-def f_index():                                                                  
-    return redirect("/login")
+
+@app.route("/", methods = ['GET'])
+@app.route("/index", methods = ['GET'])
+def f_index():
+    if request.method == "GET":
+        return render_template("index.html")
+    else:
+        return abort(405)
