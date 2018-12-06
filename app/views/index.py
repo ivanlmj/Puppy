c1e7710386aa097a529d26033ddbc5ab415534fa
ivanlmj <ivanlmj@gmail.com>
2018-12-06 17:06:00 -0600

    adding session cookie verification

--- app/views/index.py
+++ app/views/index.py
@@ -1,12 +1,14 @@
-""" Module for '/' route """
+""" Module for '/' and '/index' routes. """
 
-from app import app, abort, render_template, request
+from app import app, abort, redirect, request, session
 
 
 @app.route("/", methods = ['GET'])
 @app.route("/index", methods = ['GET'])
 def f_index():
     if request.method == "GET":
-        return render_template("index.html")
+        if "logged_in" in session:
+            return redirect("/panel")
+        return redirect("/login")
     else:
         return abort(405)
