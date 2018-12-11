49859c4371db5964cda292d0b5f7772811db37b7
ivanlmj <ivanlmj@gmail.com>
2018-12-11 10:21:30 -0600

    redefining CRUD concept throughout the module
    - methods now returning strings or lists
    - adding and improving docstrings
    - __init__(): has optional param for redefining db path (database
      module)
    - create(): verifying lastrowid of inserted record
    - read(): formed show() method
    - update(): fixing UpdateTime timestamp (adding datetime) + verifying
      record update
    - delete(): adding verification (if record was really deleted)
    - history(): removing list comprehension (default return from fetchall()
      is sufficient)

--- app/modules/actions.py
+++ app/modules/actions.py
@@ -1,77 +1,133 @@
 #!/usr/bin/python3
 
+import datetime
 import subprocess as sp
 from app.modules import database
 
 
 class Action:
 
-    def __init__(self):
+    def __init__(self, subdir=None):
+        """ Creates an instance for SQLite class.
+
+        Parameters
+            subdir : str
+                defines a subdir for SQLite connector(), in order to build
+                the connection URI for database path (file).
+
+        """
         self.db = database.SQLite()
+        if subdir is not None:
+            self.db.subdir = subdir
+        self.db.connector()
 
 
-    def create(self, name, action):
+    def create(self, name, command):
         """
             Creates a new action.
 
             Parameters:
-                - name:   action name
-                - action: command that executes the action
+                - name:    action name
+                - command: command that executes the action
+
+            Returns:
+                - status (str) : OK or NOK
         """
         try:
             cursor = self.db.conn.cursor()
-            query = """ INSERT INTO Actions (Name, Action) VALUES
-                        ('{0}', '{1}') """.format(name, action)
-            cursor.execute(query)
+            create_action = """ INSERT INTO Actions (Name, Action) VALUES
+                                ('{0}', '{1}') """.format(name, command)
+            cursor.execute(create_action)
+            row_id = cursor.lastrowid
             self.db.conn.commit()
-            return 0
+            if type(row_id) is int:
+                return "OK"
+            else:
+                return "NOK"
         except Exception as e:
             return e
 
 
-    def update(self, action_id, name, action):
+    def read(self, action_id=None):
+        """
+            Retrieves a list of created actions.
+
+            Parameters:
+                - action_id (opt): Id for specific action
+
+            Returns:
+                - data (list) : created actions
+        """
+        try:
+            cursor = self.db.conn.cursor()
+            data = None
+            if action_id is None:
+                read_actions = """ SELECT * FROM Actions """
+                cursor.execute(read_actions)
+                data = cursor.fetchall()
+            else:
+                read_actions = """ SELECT * FROM Actions
+                                   WHERE Id='{0}'
+                                   ORDER BY Id DESC """.format(action_id)
+                cursor.execute(read_actions)
+                data = cursor.fetchall()
+                if len(data) > 0:
+                    data = [row for row in data[0]]
+            print("Read: ", data)
+            self.db.conn.commit()
+            return data
+        except Exception as e:
+            return e
+
+
+    def update(self, action_id, name, command):
         """
             Updates an existent action.
 
             Parameters:
                 - action_id: Id of the action that is going to be updated
+
+            Returns:
+                - status (str) : OK or NOK
         """
         try:
+            ts = datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
             cursor = self.db.conn.cursor()
-            query = """ UPDATE Actions SET Name={0}, Action={1}
-                        WHERE Id={2}""".format(name, action, action_id)
-            cursor.execute(query)
+            update_action = """ UPDATE Actions
+                        SET Name='{0}', Action='{1}', UpdateTime='{3}'
+                        WHERE Id='{2}' """.format(name, command, action_id, ts)
+            cursor.execute(update_action)
             self.db.conn.commit()
-            return 0
+            check_update = self.read(action_id)
+            if check_update[3] < check_update[4]:
+                return "OK"
+            else:
+                return "NOK"
         except Exception as e:
             return e
 
 
     def delete(self, action_id):
         """
-            Removes an action.
+            Deletes an action.
 
             Parameters:
                 - action_id: Id of the Action
 
             Return:
-                - 0: action removed
-                - 1: action not removed
+                - status (str) : OK or NOK
         """
         try:
             cursor = self.db.conn.cursor()
-            query = """ DELETE FROM Actions WHERE Id='{0}'""".format(name)
-            cursor.execute(query)
+            delete_action = """ DELETE FROM Actions
+                                WHERE Id='{0}'""".format(action_id)
+            cursor.execute(delete_action)
             self.db.conn.commit()
-            check_remove = """ SELECT * FROM Actions
-                               WHERE Id='{0}'""".format(name)
-            cursor.execute(check_remove)
-            data = cursor.fetchall()
-            self.db.conn.commit()
-            if len(data) == 0:
-                return 0
+            check_delete = self.read(action_id)
+            if len(check_delete) == 0:
+                return "OK"
             else:
-                return 1
+                return "NOK"
         except Exception as e:
             return e
 
@@ -85,9 +141,9 @@ class Action:
         """
         try:
             cursor = self.db.conn.cursor()
-            query_action = """ SELECT Name, Action FROM Actions
-                               WHERE Id='{0}'""".format(action_id)
-            cursor.execute(query_action)
+            run_action = """ SELECT Name, Action FROM Actions
+                             WHERE Id='{0}' """.format(action_id)
+            cursor.execute(run_action)
             action_data = cursor.fetchall()[0]
             self.db.conn.commit()
             action  = action_data[0]
@@ -98,40 +154,18 @@ class Action:
             # 2 - sanitize command for cases like "rm -rf /"
             result  = sp.call(command.split())
             if result == 0:
-                query_action_log  = """ INSERT INTO ActionsHistory
+                create_history  = """ INSERT INTO ActionsHistory
                 (Name, ReturnCode, RunBy) VALUES('{0}', '{1}', '{2}')
                 """.format(action, result, user)
-                cursor.execute(query_action_log)
+                cursor.execute(create_history)
+                row_id = cursor.lastrowid
                 self.db.conn.commit()
-                return 0
+                if type(row_id) is int:
+                    return "OK"
+                else:
+                    return "NOK"
             else:
-                return 1
-        except Exception as e:
-            return e
-
-
-    def show(self, action_id=None):
-        """
-            Lists created actions (depending on optional parameter).
-
-            Parameters:
-                - action_id (opt): Id for specific action
-
-            Return:
-                - list : created actions
-        """
-        try:
-            cursor = self.db.conn.cursor()
-            query = None
-            if action_id is None:
-                query = """ SELECT * FROM Actions """
-            else:
-                query = """ SELECT * FROM Actions
-                            WHERE Id='{0}'""".format(action_id)
-            cursor.execute(query)
-            data = [ row for row in cursor.fetchall() ]
-            self.db.conn.commit()
-            return data
+                return result
         except Exception as e:
             return e
 
@@ -147,7 +181,7 @@ class Action:
             cursor = self.db.conn.cursor()
             query = """ SELECT * FROM ActionsHistory """
             cursor.execute(query)
-            data = [ row for row in cursor.fetchall() ]
+            data = cursor.fetchall()
             self.db.conn.commit()
             return data
         except Exception as e:
