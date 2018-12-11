06089403e120294ccf898135d92db2e9ce9412cd
ivanlmj <ivanlmj@gmail.com>
2018-12-11 08:34:22 -0600

    adding method for building connection object...
    - ...depending on user's current working directoty
    - defining docstrings

--- app/modules/database.py
+++ app/modules/database.py
@@ -3,9 +3,34 @@ import os.path
 import sqlite3
 
 class SQLite:
+    """ Provides methods for building SQLite3 connections for Shell-WA.
+
+        Notice:
+
+            depending on your current working directory, instance variable
+            "subdir" should be changed, before building a connection object.
+
+        When running dbsetup.py:
+
+            from app.module import database
+
+            db = database.SQLite
+            db.subdir = "/app/modules"
+            db.connector()
+            cursor = db.conn.cursor()
+            drop_users = ''' DROP TABLE IF EXISTS Users '''
+            cursor.execute(drop_users)
+
+    """
 
     def __init__(self):
-        """ Database setup. """
-        BASE_DIR = os.path.dirname(os.path.abspath(__name__)) + "/modules"
-        self.db_path = os.path.join(BASE_DIR, "puppy.sqlite3")
+        self.subdir = "/modules"
+
+    def connector(self):
+        """ Builds SQLIte URI for the database file (according to your
+        current working directory) and builds a connection object.
+
+        """
+        db_dir = os.getcwd() + self.subdir
+        self.db_path = os.path.join(db_dir, "swa.sqlite3")
         self.conn = sqlite3.connect(self.db_path)
